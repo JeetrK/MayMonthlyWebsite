4bcc062953520ced3eef1946b51ea562c7baa58f
JeetrK <526hKlein@frhsd.com>
2025-05-13 09:31:13 -0400

    start

---
+++ players.js
@@ -0,0 +1,33 @@
+const players = [
+    {
+        firstName: 'Burt',
+        lastName: 'Obeng',
+        position: 'Fish',
+        placement: '1st',
+        photo: 'https://cdn.fishtank.live/contestants/BURT-1732479633358.jpg'
+    },{
+        firstName: 'Burt',
+        lastName: 'Obeng',
+        position: 'Fish',
+        placement: '1st',
+        photo: 'https://cdn.fishtank.live/contestants/BURT-1732479633358.jpg'
+    },{
+        firstName: 'Burt',
+        lastName: 'Obeng',
+        position: 'Fish',
+        placement: '1st',
+        photo: 'https://cdn.fishtank.live/contestants/BURT-1732479633358.jpg'
+    },{
+        firstName: 'Burt',
+        lastName: 'Obeng',
+        position: 'Fish',
+        placement: '1st',
+        photo: 'https://cdn.fishtank.live/contestants/BURT-1732479633358.jpg'
+    },{
+        firstName: 'Burt',
+        lastName: 'Obeng',
+        position: 'Fish',
+        placement: '1st',
+        photo: 'https://cdn.fishtank.live/contestants/BURT-1732479633358.jpg'
+    },
+]
