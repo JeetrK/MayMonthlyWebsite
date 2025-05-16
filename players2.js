a66368f6d0631a1b2fbad20a7f89f0c5824b07ac
JeetrK <526hKlein@frhsd.com>
2025-05-16 08:58:49 -0400

    season2 added

--- players2.js
+++ players2.js
@@ -1,9 +1,129 @@
 const players = [
     {
-        firstName: 'Burt',
-        lastName: 'Obeng',
+        firstName: 'TJ',
+        lastName: 'James',
         position: 'Fish',
         placement: '1st',
-        photo: '/imgs/BURT.jpg'
+        photo: '/imgs2/TJ.png'
+    },{
+        firstName: 'Shinji',
+        lastName: 'Kawasaki',
+        position: 'Fish',
+        placement: '2nd',
+        photo: '/imgs2/SHINJI.png'
+    },{
+        firstName: 'Tayleigh',
+        lastName: 'Pendleton',
+        position: 'Fish',
+        placement: '3rd',
+        photo: '/imgs2/TAYLEIGH.png'
+    },{
+        firstName: 'Trisha',
+        lastName: 'Brielle',
+        position: 'Fish',
+        placement: '4th',
+        photo: '/imgs2/TRISHA.png'
+    },{
+        firstName: 'Jimmy',
+        lastName: 'Downey',
+        position: 'Fish',
+        placement: '5th',
+        photo: '/imgs2/JIMMY.png'
+    },{
+        firstName: 'Brian',
+        lastName: 'Maloney',
+        position: 'Fish',
+        placement: '6th',
+        photo: '/imgs2/BRIAN.png'
+    },{
+        firstName: 'Cole',
+        lastName: 'Dennis',
+        position: 'Fish',
+        placement: '7th',
+        photo: '/imgs2/COLE.png'
+    },{
+        firstName: 'Summer',
+        lastName: 'Pelkey',
+        position: 'Fish',
+        placement: '9th',
+        photo: '/imgs2/SUMMER.png'
+    },{
+        firstName: 'JC',
+        lastName: 'Chiang',
+        position: 'Fish',
+        placement: '10th',
+        photo: '/imgs2/JC.png'
+    },{
+        firstName: 'Taylor',
+        lastName: 'Lewis',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/TAYLOR.png'
+    },{
+        firstName: 'Nifty',
+        lastName: '',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/NIFTY.png'
+    },{
+        firstName: 'Abi',
+        lastName: 'Britney',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/ABI.png'
+    },{
+        firstName: 'Frank',
+        lastName: 'Hassle',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/FRANKS2.png'
+    },{
+        firstName: 'Tai',
+        lastName: 'Nguyen',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/TAI.png'
+    },{
+        firstName: 'Dunyay',
+        lastName: 'Enasche',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/DUNYAY.png'
+    },{
+        firstName: 'Oliver',
+        lastName: '',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/OLIVER.png'
+    },{
+        firstName: 'Mr.Beast/Greg',
+        lastName: '',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/GREG.png'
+    },{
+        firstName: 'Chris',
+        lastName: '',
+        position: 'Freeloader',
+        placement: '',
+        photo: '/imgs2/CHRIS.png'
+    },{
+        firstName: 'Judge Jedidiah',
+        lastName: 'Goldstriker',
+        position: 'Host',
+        placement: '',
+        photo: '/imgs2/JUDGE.png'
+    },{
+        firstName: 'Jet',
+        lastName: 'Neptune',
+        position: 'Producer',
+        placement: '',
+        photo: '/imgs2/JET2.png'
+    },{
+        firstName: 'Ben',
+        lastName: 'Taylor',
+        position: 'Producer',
+        placement: '',
+        photo: '/imgs2/BEN.png'
     },
 ]
