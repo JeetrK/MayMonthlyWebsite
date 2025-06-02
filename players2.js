bf250246f7d0cd54a109064855010440f19b9840
JeetrK <526HKlein@frhsd.com>
2025-06-02 17:14:00 +0000

    fixed images

--- players2.js
+++ players2.js
@@ -5,7 +5,7 @@ const players = [
         lastName: 'James',
         position: 'Fish',
         placement: '1st',
-        photo: '/imgs2/TJ.png',
+        photo: 'imgs2/TJ.png',
         hiddenDetail: 'TJ, also known as Thomas, is a contestant who appeared on season 2 of Fishtank. The winner of season 2, he won in the finale against Shinji and Tayleigh.'
     },{
         id: '20',
@@ -13,7 +13,7 @@ const players = [
         lastName: 'K',
         position: 'Fish',
         placement: '2nd',
-        photo: '/imgs2/SHINJI.png',
+        photo: 'imgs2/SHINJI.png',
         hiddenDetail: 'Shinji Kawasaki is a contestant who appeared on season 2 of Fishtank. He finished as the runner-up in 2nd place despite barely knowing english. He is the second most endorsed fish in fishtank history despite losing in the finale to the winner, TJ.'
     },{
         id: '21',
@@ -21,7 +21,7 @@ const players = [
         lastName: 'P',
         position: 'Fish',
         placement: '3rd',
-        photo: '/imgs2/TAYLEIGH.png',
+        photo: 'imgs2/TAYLEIGH.png',
         hiddenDetail: 'Tayleigh, also known as Tay, is a contestant who appeared on season 2 and season 2.5 of Fishtank. In season 2, she finished in 3rd place during the finale.'
     },{
         id: '22',
@@ -29,7 +29,7 @@ const players = [
         lastName: 'Brielle',
         position: 'Fish',
         placement: '4th',
-        photo: '/imgs2/TRISHA.png',
+        photo: 'imgs2/TRISHA.png',
         hiddenDetail: 'Trisha Brielle, also known as Trish, is a contestant who appeared on season 2 of Fishtank. She finished in 4th place. She is the only fish ever that was allowed to stay in the house after elimination.'
     },{
         id: '23',
@@ -37,7 +37,7 @@ const players = [
         lastName: 'Downey',
         position: 'Fish',
         placement: '5th',
-        photo: '/imgs2/JIMMY.png',
+        photo: 'imgs2/JIMMY.png',
         hiddenDetail: 'Jimmy Downey was a contestant on fishtank known for his weird personality and anger issues. He was kickced off the show after throwing a mallot at Abi over a toy phone she took from him.'
     },{
         id: '24',
@@ -45,7 +45,7 @@ const players = [
         lastName: 'Maloney',
         position: 'Fish',
         placement: '6th',
-        photo: '/imgs2/BRIAN.png',
+        photo: 'imgs2/BRIAN.png',
         hiddenDetail: 'Brian Maloney is a contestant who appeared on season 2 of Fishtank and finished in 6th place. He was kicked off for not trying hard enough'
     },{
         id: '25',
@@ -53,7 +53,7 @@ const players = [
         lastName: 'Dennis',
         position: 'Fish',
         placement: '7th',
-        photo: '/imgs2/COLE.png',
+        photo: 'imgs2/COLE.png',
         hiddenDetail: 'Cole Dennis is a contestant who appeared on season 2 of Fishtank. He finished in 7th place. He was kicked off the show for reasons never really explained, some speculate he was kicked for talking about season 1 and not listening to production.'
     },{
         id: '26',
@@ -61,7 +61,7 @@ const players = [
         lastName: 'P',
         position: 'Fish',
         placement: '9th',
-        photo: '/imgs2/SUMMER.png',
+        photo: 'imgs2/SUMMER.png',
         hiddenDetail: 'Summer Pelkey is a contestant who appeared on season 2 of Fishtank. She finished in 10th place. She was removed from the show by production because she was not mentally well enough to be on the show, after being eliminated she was sent to a mental hospital and continued to try and get back on the show. she sent production 40 minutes worth of voice memos that contained insane ramblings about nothing.'
     },{
         id: '27',
@@ -69,7 +69,7 @@ const players = [
         lastName: 'Chiang',
         position: 'Fish',
         placement: '10th',
-        photo: '/imgs2/JC.png',
+        photo: 'imgs2/JC.png',
         hiddenDetail: 'JC Chiang is a contestant who appeared on season 2 of Fishtank. She finished in 9th place. She is the youngest contestant to ever be on fishtank and was the only fish in season 2 who didnt know anything about the show, she left after the first challenge.'
     },{
         id: '28',
@@ -77,7 +77,7 @@ const players = [
         lastName: 'Lewis',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/TAYLOR.png',
+        photo: 'imgs2/TAYLOR.png',
         hiddenDetail: 'Taylor is a freeloader who appeared on season 2 of Fishtank. She joined the house on day 11, and was removed on day 15.'
     },{
         id: '29',
@@ -85,7 +85,7 @@ const players = [
         lastName: '',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/NIFTY.png',
+        photo: 'imgs2/NIFTY.png',
         hiddenDetail: 'Nifty is a freeloader who appeared in season 2 and season 3 of Fishtank. She was brought on the show purely because she was annoying.'
     },{
         id: '30',
@@ -93,7 +93,7 @@ const players = [
         lastName: 'Britney',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/ABI.png',
+        photo: 'imgs2/ABI.png',
         hiddenDetail: 'Abi made her debut on day 20 of season 2, entering the house under the pseudonym Britney. She quickly developed a complex relationship with Jimmy.'
     },{
         id: '15',
@@ -101,7 +101,7 @@ const players = [
         lastName: 'Hassle',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/FRANKS2.png',
+        photo: 'imgs2/FRANKS2.png',
         hiddenDetail: 'Frank Hassle is a controversial internet personality and content creator known for his confrontational and provocative public videos. He was introduced to fishtank in season one and became a reocurring freeloader every season. He is put into the house usually twoards the end with the main goal of harrasing the fish and pushing everyone to their limit.'
     },{
         id: '31',
@@ -109,7 +109,7 @@ const players = [
         lastName: 'Nguyen',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/TAI.png',
+        photo: 'imgs2/TAI.png',
         hiddenDetail: 'Tai Nguyen, also known as Simon, is a freeloader who appeared on season 1 and season 2 of Fishtank. He also competed on season 2.5. he was kicked off season 2 because he was winning too much.'
     },{
         id: '32',
@@ -117,7 +117,7 @@ const players = [
         lastName: 'E',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/DUNYAY.png',
+        photo: 'imgs2/DUNYAY.png',
         hiddenDetail: 'Duanye, also spelled Duanyay, is a freeloader who appeared on season 2 of Fishtank. He holds the record for the longest running freeloader on fishtank and stayed until the end.'
     },{
         id: '33',
@@ -125,7 +125,7 @@ const players = [
         lastName: '',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/OLIVER.png',
+        photo: 'imgs2/OLIVER.png',
         hiddenDetail: 'Oliver is a freeloader who appeared on Season 2 of Fishtank. He entered the house on January 9 and remained there until the finale on January 28.'
     },{
         id: '34',
@@ -133,7 +133,7 @@ const players = [
         lastName: '',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/GREG.png',
+        photo: 'imgs2/GREG.png',
         hiddenDetail: 'Greg LeCates, also known as Fake Mr. Beast or Skitzy, is a YouTuber/Streamer who is well-known for portraying MrBeast in the Squid Game vs MrBeast rap battle. He was brought onto the show to do his mrbeast bit and was supposed to be there for one day only but production kept convincing him to stay despite him contantly wanting to leave, he ended up staying until the end of the show.'
     },{
         id: '35',
@@ -141,7 +141,7 @@ const players = [
         lastName: '',
         position: 'Freeloader',
         placement: 'none',
-        photo: '/imgs2/CHRIS.png',
+        photo: 'imgs2/CHRIS.png',
         hiddenDetail: 'Chris, better known online as Airsoftfatty, is an American YouTube vlogger and comedian. He is a freeloader who comes back every season and is a fishtank staple for his tantrums and excentric personality.'
     },{
         id: "16",
@@ -149,7 +149,7 @@ const players = [
         lastName: 'Goldstriker',
         position: 'Host',
         placement: 'none',
-        photo: '/imgs2/JUDGE.png',
+        photo: 'imgs2/JUDGE.png',
         hiddenDetail: 'Real name Sam Hyde, is an American comedian. He is a co-founder of the sketch comedy group Million Dollar Extreme (MDE), alongside Nick Rochefort and Charls Carroll. Hyde co-created produced the series World Peace, and was a host of the reality series Fishtank.'
     },{
         id: "17",
@@ -157,7 +157,7 @@ const players = [
         lastName: 'Neptune',
         position: 'Producer',
         placement: 'none',
-        photo: '/imgs2/JET2.png',
+        photo: 'imgs2/JET2.png',
         hiddenDetail: 'Jet Neptune is the co-creator, director and an executive producer of Fishtank. He is also credited as an editor for the shows edited episodes'
     },{
         id: '18',
@@ -165,7 +165,7 @@ const players = [
         lastName: 'Taylor',
         position: 'Producer',
         placement: 'none',
-        photo: '/imgs2/BEN.png',
+        photo: 'imgs2/BEN.png',
         hiddenDetail: 'Benjamin Taylor, also known as Ben Bush, is a producer for Fishtank'
     },
 ]
